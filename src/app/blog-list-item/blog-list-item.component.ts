5d357a143141a951753f597216d34b86796b67ee
Omar Zidan <w0878888@myscc.ca>
2025-10-03 10:38:57 -0400

    Added @Input() property to BlogListItemComponent

--- src/app/blog-list-item/blog-list-item.component.ts
+++ src/app/blog-list-item/blog-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import { BlogPost } from '../models/blog-post';
 
 @Component({
   selector: 'app-blog-list-item',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './blog-list-item.component.css'
 })
 export class BlogListItemComponent {
-
+  @Input() post?: BlogPost;
 }
