5798b06929fe6fbd369a8022a6717a809bba023b
Omar Zidan <w0878888@myscc.ca>
2025-10-03 09:57:22 -0400

    Create blog-list and blog-list-item components

---
+++ src/app/blog-list-item/blog-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-list-item',
+  imports: [],
+  templateUrl: './blog-list-item.component.html',
+  styleUrl: './blog-list-item.component.css'
+})
+export class BlogListItemComponent {
+
+}
