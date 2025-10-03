f572eb0ef361de07e65f96f8dc5ca2bd58a9dc5c
Omar Zidan <w0878888@myscc.ca>
2025-10-03 09:46:26 -0400

    Assignment2 Completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlogPost} from './models/blog-post';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('Personal-Blog');
+
+  // I will fill them later
+  blogPostList: BlogPost[] = [
+    { id: 1, title: "Post One", content: "Content 1", author: "Author A", published: true },
+    { id: 2, title: "Post Two", content: "Content 2", author: "Author B" },
+    { id: 3, title: "Post Three", content: "Content 3", author: "Author C", published: false },
+    { id: 4, title: "Post Four", content: "Content 4", author: "Author D", published: true },
+    { id: 5, title: "Post Five", content: "Content 5", author: "Author E" },
+    { id: 6, title: "Post Six", content: "Content 6", author: "Author F", published: true }
+  ];
+
+  togglePublished(post: BlogPost): void {
+    post.published = !post.published;
+  }
 }
+
