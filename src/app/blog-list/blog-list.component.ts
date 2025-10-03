1607777de200b1a632ff26804ae04e989354ee76
Omar Zidan <w0878888@myscc.ca>
2025-10-03 10:35:09 -0400

    Added array of blog posts to BlogListComponent

--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BlogPost } from '../models/blog-post';
 
 @Component({
   selector: 'app-blog-list',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './blog-list.component.css'
 })
 export class BlogListComponent {
+  contentList: BlogPost[] = [
+    { id: 1, title: "Post 1", content: "This is content 1", author: "Omar" },
+    { id: 2, title: "Post 2", content: "This is content 2", author: "Omar" },
+    { id: 3, title: "Post 3", content: "This is content 3", author: "Omar" },
+    { id: 4, title: "Post 4", content: "This is content 4", author: "Omar" }
+  ];
 
 }
