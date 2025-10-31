47ff5aa069fe6aa9c7fa35bd49d91e486fe2fd0b
Omar Zidan <w0878888@myscc.ca>
2025-10-31 11:27:51 -0400

    Fixed import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import { BlogPost} from './models/blog-post';
 import {BlogListComponent} from './blog-list/blog-list.component';
 import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [BlogListComponent, NgForOf, RouterOutlet],
+  imports: [BlogListComponent, NgForOf, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
