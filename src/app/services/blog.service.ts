9f50ce204d54c3d7ec5fe64c0eb696943ae10e02
Omar Zidan <w0878888@myscc.ca>
2025-11-07 11:25:16 -0500

    Assignment7: Updated blog.service.ts so it is using HttpClient for CRUD operations :D

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,48 +1,71 @@
 import { Injectable } from '@angular/core';
 import { BlogPost } from '../models/blog-post';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
+  private postsUrl = 'api/blogPosts';
 
-  private contentList: BlogPost[] = [
+
+  private blogPosts: BlogPost[] = [
     { id: 1, title: "Post 1", content: "This is content 1", author: "Omar", imageUrl: "https://picsum.photos/id/1/400/250" },
     { id: 2, title: "Post 2", content: "This is content 2", author: "Omar", imageUrl: "https://picsum.photos/id/2/400/250" },
     { id: 3, title: "Post 3", content: "This is content 3", author: "Omar", imageUrl: "https://picsum.photos/id/3/400/250" },
     { id: 4, title: "Post 4", content: "This is content 4", author: "Omar", imageUrl: "https://picsum.photos/id/4/400/250" }
   ];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
 
   getPosts(): Observable<BlogPost[]> {
-    return of(this.contentList);
+    return this.http.get<BlogPost[]>(this.postsUrl)
+      .pipe(catchError(this.handleError));
   }
 
-  getPostById(id: number): Observable<BlogPost | undefined> {
-    return of(this.contentList.find(post => post.id === id));
+
+  getPostById(id: number): Observable<BlogPost> {
+    const url = `${this.postsUrl}/${id}`;
+    return this.http.get<BlogPost>(url)
+      .pipe(catchError(this.handleError));
   }
+
 
   addPost(post: BlogPost): Observable<BlogPost> {
-    this.contentList.push(post);
-    return of(post);
+
+    post.id = this.generateNewId();
+
+    this.blogPosts.push(post);
+    return this.http.post<BlogPost>(this.postsUrl, post)
+      .pipe(catchError(this.handleError));
   }
 
-  updatePost(updatedPost: BlogPost): Observable<BlogPost | undefined> {
-    const index = this.contentList.findIndex(post => post.id === updatedPost.id);
-    if (index > -1) {
-      this.contentList[index] = updatedPost;
-      return of(updatedPost);
-    }
-    return of(undefined); // Return undefined if the post wasn't found
+
+  updatePost(updatedPost: BlogPost): Observable<any> {
+    return this.http.put(this.postsUrl, updatedPost)
+      .pipe(catchError(this.handleError));
   }
 
-  deletePost(id: number): void {
-    this.contentList = this.contentList.filter(post => post.id !== id);
+
+  deletePost(id: number): Observable<{}> {
+    const url = `${this.postsUrl}/${id}`;
+
+    this.blogPosts = this.blogPosts.filter(p => p.id !== id);
+    return this.http.delete(url)
+      .pipe(catchError(this.handleError));
   }
+
 
   generateNewId(): number {
-    return this.contentList.length > 0 ? Math.max(...this.contentList.map(post => post.id)) + 1 : 1;
+    return this.blogPosts.length > 0 ? Math.max(...this.blogPosts.map(post => post.id)) + 1 : 1;
+  }
+
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('API error:', error);
+
+    return throwError(() => new Error('A data error occurred, please try again.'));
   }
 }
