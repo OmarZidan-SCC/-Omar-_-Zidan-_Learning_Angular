ab2faa44963a3ce17201517604a769cd38d551d2
Omar Zidan <w0878888@myscc.ca>
2025-11-28 05:14:47 -0500

    Assignment10: Changed package.json to version 0.8.0

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component, signal } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import { BlogPost} from './models/blog-post';
 
 
 @Component({
